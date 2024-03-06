5cc3a39e798c7463534ac3adb5ff6b293aac1517
David Carlier <devnexen@gmail.com>
2024-03-06 22:56:58 +0000

    trying out new option before merged to master.

--- libmimalloc-sys/build.rs
+++ libmimalloc-sys/build.rs
@@ -32,6 +32,12 @@ fn main() {
         }
     }
 
+    if env::var_os("CARGO_FEATURE_NO_THP").is_some() {
+        if target_os == "linux" {
+            build.define("MI_NO_THP", "1");
+        }
+    }
+
     if env::var_os("CARGO_FEATURE_DEBUG").is_some()
         || (env::var_os("CARGO_FEATURE_DEBUG_IN_DEBUG").is_some() && cfg!(debug_assertions))
     {
